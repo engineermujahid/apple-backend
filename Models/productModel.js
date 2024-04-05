072eacb55d86a766e1a940702d0f8a860d4c3ca3
engineermujahid <engineermujahid04@gmail.com>
2024-04-05 15:51:20 +0500

    Final Chnges implemented

--- Models/productModel.js
+++ Models/productModel.js
@@ -8,7 +8,7 @@ const productSchema = new mongoose.Schema(
         brand: { type: String, required: true },
         classification: { type: String, required: true },
         rating: { type: Number, min: [0, "Wrong Minimum rating"], max: [5, "Wrong Maximum rating"], required: true },
-        thumbnail: { type: String, required: true },
+        thumbnail: { type: String },
         stock: { type: Number, required: true },
         images: { type: [String] },
     },
